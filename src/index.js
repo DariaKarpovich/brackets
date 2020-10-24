a01ef301c086548b807e76f8f46780610b62fedd
DariaKarpovich <dashka230@yandex.ru>
2020-10-24 22:48:36 +0300

    feat: add task files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let objConf = {}
+
+  bracketsConfig.map(element => {
+    objConf[element[1]] = element[0]
+  });
+
+  for(let i = 0; i < str.length; i++) {
+    if (objConf[str[i]] && objConf[str[i]] === stack[stack.length - 1]) { 
+      stack.pop()
+    } else {
+        stack.push(str[i])
+    }
+  }
+
+  return stack.length === 0;
 }
